2291fc0123429c79e0d9a6d8f63f290afa8cc6f4
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2021-11-19 18:53:56 +0100

    update express urlencoded in app.js

--- app.js
+++ app.js
@@ -6,18 +6,27 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 // MIDDLEWARES
-app.use(urlencoded({extended: true}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// CORS CONFIGURATION 
+app.use(function (req, res, next) {
 
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.options('*', cors(corsOptions))
+    // Website /origins allowed to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods allowed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers allowed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 
 // DATABASE CONNECTION 
 mongoose.connect(process.env.MONGODB_URI,
